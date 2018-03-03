925325adf6b6e4da671b6a0eda26d54d0ef08819
Rowinbot <rowinkaleb@gmail.com>
2018-03-02 19:08:19 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+const MI_NOMBRE = "Rowin"
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hola soy {MI_NOMBRE}
+      </div>
+    );
+  }
+}
+
+export default App;
